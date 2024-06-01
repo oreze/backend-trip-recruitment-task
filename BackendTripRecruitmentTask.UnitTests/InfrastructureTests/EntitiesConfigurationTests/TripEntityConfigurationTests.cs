2694f5c9ef678480f3a173ac6dcbc3d94c5e6800
Szymon Jędrych <22376195+oreze@users.noreply.github.com>
2024-06-01 19:57:06 +0200

    Add trip entity configuration tests

---
+++ BackendTripRecruitmentTask.UnitTests/InfrastructureTests/EntitiesConfigurationTests/TripEntityConfigurationTests.cs
@@ -0,0 +1,60 @@
+using BackendTripRecruitmentTask.Domain.Entities;
+using BackendTripRecruitmentTask.Infrastructure.Data;
+using BackendTripRecruitmentTask.Infrastructure.EntitiesConfiguration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+namespace BackendTripRecruitmentTask.UnitTests.InfrastructureTests.EntitiesConfigurationTests;
+
+public class TripEntityConfigurationTests
+{
+    [Fact]
+    public void Configure_SetsConfigurationProperly()
+    {
+        var options = new DbContextOptionsBuilder<TripDbContext>()
+            .UseInMemoryDatabase("test_database")
+            .Options;
+
+        using var dbContext = new TripDbContext(options);
+        var builder = new ModelBuilder(new ConventionSet());
+        var configuration = new TripEntityConfiguration();
+
+        configuration.Configure(builder.Entity<Trip>());
+
+        var entityType = dbContext.Model.FindEntityType(typeof(Trip));
+        Assert.NotNull(entityType);
+
+        var idProperty = entityType.FindProperty(nameof(Trip.ID));
+        Assert.NotNull(idProperty);
+        Assert.True(idProperty.IsPrimaryKey());
+
+        var nameProperty = entityType.FindProperty(nameof(Trip.Name));
+        Assert.NotNull(nameProperty);
+        Assert.Equal(50, nameProperty.GetMaxLength());
+        Assert.False(nameProperty.IsNullable);
+        Assert.True(nameProperty.IsUniqueIndex());
+
+        var descriptionProperty = entityType.FindProperty(nameof(Trip.Description));
+        Assert.NotNull(descriptionProperty);
+
+        var startDateProperty = entityType.FindProperty(nameof(Trip.StartDate));
+        Assert.NotNull(startDateProperty);
+        Assert.False(startDateProperty.IsNullable);
+
+        var numberOfSeatsProperty = entityType.FindProperty(nameof(Trip.NumberOfSeats));
+        Assert.NotNull(numberOfSeatsProperty);
+        Assert.False(numberOfSeatsProperty.IsNullable);
+
+        var registrationsNavigation = entityType.FindNavigation(nameof(Trip.Registrations));
+        Assert.NotNull(registrationsNavigation);
+        Assert.True(registrationsNavigation.IsCollection);
+        Assert.Equal(nameof(Registration.TripID), registrationsNavigation.ForeignKey.Properties.Single().Name);
+        Assert.True(registrationsNavigation.ForeignKey.IsRequired);
+
+        var countryNavigation = entityType.FindNavigation(nameof(Trip.Country));
+        Assert.NotNull(countryNavigation);
+        Assert.False(countryNavigation.IsCollection);
+        Assert.Equal(nameof(Trip.CountryThreeLetterCode), countryNavigation.ForeignKey.Properties.Single().Name);
+        Assert.True(countryNavigation.ForeignKey.IsRequired);
+    }
+}
