e67beb948f29316e6718b3edb9ff77a578f38278
Szymon Jędrych <22376195+oreze@users.noreply.github.com>
2024-06-02 17:59:50 +0200

    Implement batch of CreateTrip method tests

--- BackendTripRecruitmentTask.IntegrationTests/Features/TripTests.cs
+++ BackendTripRecruitmentTask.IntegrationTests/Features/TripTests.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using System.Net.Http.Json;
 using System.Text;
 using System.Text.Json;
@@ -14,11 +15,9 @@ public class TripTests : IClassFixture<WebApplicationFactory<Program>>
     private readonly TripDbContext _dbContext;
     private readonly IServiceScope _scope;
     private readonly HttpClient _httpClient;
-    private readonly WebApplicationFactory<Program> _webApplicationFactory;
 
     public TripTests(WebApplicationFactory<Program> factory)
     {
-        _webApplicationFactory = factory;
         _httpClient = factory.CreateDefaultClient();
         _scope = factory.Services.CreateScope();
         _dbContext = _scope.ServiceProvider.GetRequiredService<TripDbContext>();
@@ -34,7 +33,7 @@ public class TripTests : IClassFixture<WebApplicationFactory<Program>>
     public async Task CreateTrip_ValidInput_ReturnsOk()
     {
         var createTripDto = new CreateTripDto(
-            "Trip1",
+            Guid.NewGuid().ToString(),
             "Random description",
             DateTime.UtcNow.AddDays(10),
             50, 
@@ -47,7 +46,7 @@ public class TripTests : IClassFixture<WebApplicationFactory<Program>>
 
         response.EnsureSuccessStatusCode();
         var result = await response.Content.ReadFromJsonAsync<int>();
-        Assert.Equal(1, result);
+        Assert.NotEqual(0, result);
 
         var trip = await _dbContext.Trips
             .Include(x => x.Country)
@@ -58,5 +57,73 @@ public class TripTests : IClassFixture<WebApplicationFactory<Program>>
         Assert.Equal(createTripDto.CountryName, trip!.Country.Name);
         Assert.Equal(createTripDto.NumberOfSeats, trip!.NumberOfSeats);
         Assert.Equal(createTripDto.StartDate, trip!.StartDate);
+    }
+    
+    [Fact]
+    public async Task CreateTrip_TripWithNameExists_ReturnsBadRequest()
+    {
+        var randomTripName = Guid.NewGuid().ToString();
+        var createTripDto = new CreateTripDto(
+            randomTripName,
+            "Random description",
+            DateTime.UtcNow.AddDays(10),
+            50, 
+            "Poland");
+        
+        var createTripWithSameNameDto = new CreateTripDto(
+            randomTripName,
+            "Random description2",
+            DateTime.UtcNow.AddDays(5),
+            10, 
+            "Poland");
+
+        var json = JsonSerializer.Serialize(createTripDto);
+        var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+        var response = await _httpClient.PostAsync("/trips", content);
+
+        response.EnsureSuccessStatusCode();
+
+        var secondTripJson = JsonSerializer.Serialize(createTripWithSameNameDto);
+        var secondTripContent = new StringContent(secondTripJson, Encoding.UTF8, "application/json");
+        var secondTripResponse = await _httpClient.PostAsync("/trips", secondTripContent);
+
+        Assert.Equal(HttpStatusCode.BadRequest, secondTripResponse.StatusCode);
+    }
+    
+    [Fact]
+    public async Task CreateTrip_CountryDoesNotExists_ReturnsBadRequest()
+    {
+        var createTripDto = new CreateTripDto(
+            Guid.NewGuid().ToString(),
+            "Random description",
+            DateTime.UtcNow.AddDays(10),
+            50, 
+            "Random Country That Does Not Exists");
+
+        var json = JsonSerializer.Serialize(createTripDto);
+        var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+        var response = await _httpClient.PostAsync("/trips", content);
+
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+    }
+    
+    [Fact]
+    public async Task CreateTrip_InvalidInput_ReturnsBadRequest()
+    {
+        var createTripDto = new CreateTripDto(
+            "",
+            "",
+            DateTime.UtcNow.AddDays(10),
+            2137, 
+            "Poland");
+
+        var json = JsonSerializer.Serialize(createTripDto);
+        var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+        var response = await _httpClient.PostAsync("/trips", content);
+
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
     }
 }
